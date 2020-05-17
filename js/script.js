88835f3c6f2049387ec1779b4b945becad75d424
Kendra Neves <kendraneves@gmail.com>
2020-05-17 18:34:16 -0400

    font changed, navbar background changed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $(".card-columns").append(
     `
         <div class="card bg-ltblue mb-3 shadow-lg">
-          <img class="card-img-top" src="${card.image}" alt="${card.imageAlt}">
+          <a href="${card.link}"><img class="card-img-top" src="${card.image}" alt="${card.imageAlt}"></a>
           <div class="card-body">
             <h5 class="card-title"><a class="text-brown title" target="_blank" href="${card.link}">${card.title}</a></h5>
             <p class="card-text text-blue info">${card.info}</p>
@@ -28,29 +28,140 @@ let cardList = [
     "imageAlt": "nutmeg concrete website",
     "link": "https://nutmegconcrete.com/",
     "info": "This card has supporting text below as a natural lead-in to additional content",
-    "gitHubLink": "blahblahblah",
+    "gitHubLink": "",
     "datePosted": "Posted December 11, 2019"
   },
 
   {
-  "title":"",
-  "image": "",
-  "imageAlt": "",
-  "link": "",
-  "info": "",
-  "gitHubLink": "",
-  "datePosted": ""
+  "title":"Password Generator",
+  "image": "../assets/password-generator.PNG",
+  "imageAlt": "password generator website",
+  "link": "https://kneves1.github.io/03-Password-Generator/",
+  "info": "Hello World!",
+  "gitHubLink": "https://github.com/kneves1/03-Password-Generator",
+  "datePosted": "Posted February 14, 2020"
   },
 
   {
-    "title":"",
-    "image": "",
-    "imageAlt": "",
-    "link": "",
-    "info": "",
-    "gitHubLink": "",
-    "datePosted": ""
-  }
+    "title":"Code Quiz",
+    "image": "../assets/code-quiz.PNG",
+    "imageAlt": "code quiz website",
+    "link": "https://kneves1.github.io/04-Code-Quiz/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/04-Code-Quiz",
+    "datePosted": "Posted February 21, 2020"
+  },
+
+  {
+    "title":"Work Day Scheduler",
+    "image": "../assets/work-day-scheduler.PNG",
+    "imageAlt": "work day scheduler app",
+    "link": "https://kneves1.github.io/05-Work-Day-Scheduler/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/05-Work-Day-Scheduler",
+    "datePosted": "Posted February 29, 2020"
+  },
+
+  {
+    "title":"Weather Dashboard",
+    "image": "../assets/weather-dashboard.PNG",
+    "imageAlt": "weather dashboard app",
+    "link": "https://kneves1.github.io/06-Weather-Dashboard/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/06-Weather-Dashboard",
+    "datePosted": "Posted March 9, 2020"
+  },
+
+  {
+    "title":"Good Morning App",
+    "image": "../assets/good-morning-app.PNG",
+    "imageAlt": "good morning app",
+    "link": "https://kneves1.github.io/Best-Group-Repo/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/Best-Group-Repo",
+    "datePosted": "Posted March 11, 2020"
+  },
+
+  {
+    "title":"README Generator",
+    "image": "../assets/#",
+    "imageAlt": "README generator",
+    "link": "https://github.com/kneves1/09-README-Generator",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/09-README-Generator",
+    "datePosted": "Posted March 29, 2020"
+  },
+
+  {
+    "title":"Team Profile Generator",
+    "image": "../assets/#",
+    "imageAlt": "team profile generator",
+    "link": "https://github.com/kneves1/10-Team-Profile-Generator",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/10-Team-Profile-Generator",
+    "datePosted": "Posted April 5, 2020"
+  },
+
+  {
+    "title":"Note Taking App",
+    "image": "../assets/note-taker2.PNG",
+    "imageAlt": "note taking app",
+    "link": "https://secret-springs-62228.herokuapp.com/notes",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/Express-Note-Taker",
+    "datePosted": "Posted April 13, 2020"
+  },
+
+  {
+    "title":"Employee Tracker",
+    "image": "../assets/#",
+    "imageAlt": "employee tracking app",
+    "link": "https://github.com/kneves1/Employee-Tracker",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/Employee-Tracker",
+    "datePosted": "April 20, 2020"
+  },
+
+  {
+    "title":"Burger App",
+    "image": "../assets/burger-app.PNG",
+    "imageAlt": "burger app",
+    "link": "https://calm-lake-01040.herokuapp.com/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/Node-Express-Handlebars",
+    "datePosted": "April 26, 2020"
+  },
+
+  {
+    "title":"TP Finder",
+    "image": "../assets/tp-finder1.PNG",
+    "imageAlt": "toilet paper finding app",
+    "link": "https://vast-sierra-58831.herokuapp.com/",
+    "info": "Hello World!",
+    "gitHubLink": "https://github.com/kneves1/TP-Finder",
+    "datePosted": "May 8, 2020"
+  },
+
+  // {
+  //   "title":"",
+  //   "image": "",
+  //   "imageAlt": "",
+  //   "link": "",
+  //   "info": "",
+  //   "gitHubLink": "",
+  //   "datePosted": ""
+  // },
+  
+  // {
+  //   "title":"",
+  //   "image": "",
+  //   "imageAlt": "",
+  //   "link": "",
+  //   "info": "",
+  //   "gitHubLink": "",
+  //   "datePosted": ""
+  // }
+  
 ]
 
 
